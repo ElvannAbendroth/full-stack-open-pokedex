18853a8c49b10a8f05ec2cf669ee416a1503af1d
Oodri <info@oodri.dev>
2025-01-15 15:29:48 +0200

    ⭐test workflow

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -5,7 +5,7 @@ import { useApi } from './useApi'
 import PokemonAbility from './PokemonAbility'
 import ErrorMessage from './ErrorMessage'
 
-const formatName = (nameWithDash) => nameWithDash.replace('-', ' ')
+const formatName = nameWithDash => nameWithDash.replace('-', ' ')
 
 const PokemonPage = ({ previous, next }) => {
   const { name } = useParams()
@@ -18,13 +18,15 @@ const PokemonPage = ({ previous, next }) => {
     return <ErrorMessage error={error} />
   }
 
-  const { type } = pokemon.types.find((type) => type.slot === 1)
-  const stats = pokemon.stats.map((stat) => ({
-    name: formatName(stat.stat.name),
-    value: stat.base_stat
-  })).reverse()
-  const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
-  const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
+  const { type } = pokemon.types.find(type => type.slot === 1)
+  const stats = pokemon.stats
+    .map(stat => ({
+      name: formatName(stat.stat.name),
+      value: stat.base_stat,
+    }))
+    .reverse()
+  const normalAbility = pokemon.abilities.find(ability => !ability.is_hidden)
+  const hiddenAbility = pokemon.abilities.find(ability => ability.is_hidden === true)
 
   console.log('hiddenAbility=', hiddenAbility)
   return (
@@ -32,7 +34,7 @@ const PokemonPage = ({ previous, next }) => {
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
