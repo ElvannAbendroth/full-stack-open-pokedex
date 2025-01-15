7df5949a5b628cb71f707154fe24a37fcdd942f6
Oodri <info@oodri.dev>
2025-01-15 16:51:28 +0200

    ⭐e2e workflow

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -11,3 +11,9 @@ describe('Pokedex', function () {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+it('ivysaur page can be navigated to and contains chlorophyll', function () {
+  cy.visit('http://localhost:5000')
+  cy.contains('ivysaur').click()
+  cy.contains('chlorophyll')
+})
