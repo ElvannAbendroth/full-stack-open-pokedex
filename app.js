78c75e59243ea8ae0e2c1df727a4558f35da35b1
Oodri <info@oodri.dev>
2025-01-18 14:20:46 +0200

    ⭐fly.io deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT + 1, () => {
+app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
 
@@ -15,7 +15,5 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'Error...  '
   res.send('ok')
 })
